d71cd5e3b7de8da49a0f0e5144133ab5efcd5c29
Fang Han <fhan0904@gmail.com>
2022-07-22 12:50:20 -0400

    Test vscode cmake quick start with eigen_test3

--- eigen_test2/src/eigen_test.cpp
+++ eigen_test2/src/eigen_test.cpp
@@ -1,21 +1,27 @@
 #include <Eigen/Dense>
+#include <fftw3.h>
 #include <iostream>
 
 using Eigen::MatrixXd;
 
 int main() {
   int N = 5;
-  const float pi=3.14159265358979323846;
-  Eigen::VectorXf colVec = Eigen::VectorXf::LinSpaced(N,0,N-1);
-  Eigen::VectorXf rowVec = Eigen::VectorXf::LinSpaced(N,0,N-1);
+  const float pi = 3.14159265358979323846;
+  Eigen::VectorXf colVec = Eigen::VectorXf::LinSpaced(N, 0, N - 1);
+  Eigen::VectorXf rowVec = Eigen::VectorXf::LinSpaced(N, 0, N - 1);
   Eigen::MatrixXf mn = colVec * rowVec.transpose();
-  Eigen::MatrixXf mn_2pi_overN = mn*2.0*pi/(float)N;
-  Eigen::MatrixXf M_dft_real =  mn_2pi_overN.array().cos();
-  //   MatrixXd m =
-  //       Eigen::MatrixXd::Constant(3000, 3000, 1.0);
-  //   m = m.array().cos();
-  std::cout << mn << std::endl;
-  std::cout << mn_2pi_overN << std::endl;
-  std::cout << M_dft_real << std::endl;
+  Eigen::MatrixXf mn_2pi_overN = mn * 2.0 * pi / (float)N;
+  Eigen::MatrixXf M_dft_real = mn_2pi_overN.array().cos();
+
+  fftw_complex *in, *out;
+  fftw_plan p;
+  in = (fftw_complex *)fftw_malloc(sizeof(fftw_complex) * N);
+  out = (fftw_complex *)fftw_malloc(sizeof(fftw_complex) * N);
+  p = fftw_plan_dft_1d(N, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
+  fftw_execute(p); /* repeat as needed */
+  fftw_destroy_plan(p);
+  fftw_free(in);
+  fftw_free(out);
+
   return 0;
 }
---
+++ eigen_test3/build/CMakeFiles/eigen_test3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/eigen_test3.dir/main.cpp.o"
+  "eigen_test3"
+  "eigen_test3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/eigen_test3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
